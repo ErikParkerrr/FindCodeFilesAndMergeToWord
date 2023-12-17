c9207244b5a048fb207f8f04371ab4f32a059f4e
ErikParkerrr <125108639+ErikParkerrr@users.noreply.github.com>
2023-12-17 12:02:09 -0500

    changed variable names to be plaintext generic

--- main.py
+++ main.py
@@ -5,72 +5,72 @@ from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
 FILE_EXTENSION = ".vhd"
 
-def remove_header_comments(vhdl_code):
+def remove_header_comments(plaintext_code):
    # Find the start and end markers
     start_marker = "----------------------------------------------------------------------------------"
     end_marker = "----------------------------------------------------------------------------------"
 
     # Find the indices of the start and end markers
-    start_index = vhdl_code.find(start_marker)
-    end_index = vhdl_code.find(end_marker, start_index + len(start_marker))
+    start_index = plaintext_code.find(start_marker)
+    end_index = plaintext_code.find(end_marker, start_index + len(start_marker))
 
     # Remove the text between the markers
     if start_index != -1 and end_index != -1:
-        vhdl_code = vhdl_code[:start_index] + vhdl_code[end_index + len(end_marker):]
+        plaintext_code = plaintext_code[:start_index] + plaintext_code[end_index + len(end_marker):]
 
-    return vhdl_code
+    return plaintext_code
 
-def add_vhdl_to_docx(document, vhdl_file):
+def add_plaintext_to_docx(document, plaintext_file):
     # Add file name as a heading
-    document.add_heading(f'{os.path.basename(vhdl_file)}', level=2).paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    document.add_heading(f'{os.path.basename(plaintext_file)}', level=2).paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
 
-    # Read VHDL code
-    with open(vhdl_file, 'r') as file:
-        vhdl_code = file.read()
+    # Read plaintext code
+    with open(plaintext_file, 'r') as file:
+        plaintext_code = file.read()
 
     # Remove header comments block
-    vhdl_code = remove_header_comments(vhdl_code)
+    plaintext_code = remove_header_comments(plaintext_code)
 
-    # Create a new paragraph for VHDL code
-    paragraph = document.add_paragraph(vhdl_code, style='Code')
+    # Create a new paragraph for plaintext code
+    paragraph = document.add_paragraph(plaintext_code, style='Code')
 
     # Set font size
     font = paragraph.runs[0].font
     font.size = Pt(10)
 
-def merge_vhdl_files(output_file_name="merged_vhdl.docx"):
+def merge_plaintext_files(output_file_name="merged_plaintext.docx"):
     # Get the current directory
     current_directory = os.path.dirname(os.path.abspath(__file__))
 
-    # Initialize a list to store the paths of all VHDL files
-    vhdl_files = []
+    # Initialize a list to store the paths of all plaintext files
+    plaintext_files = []
 
-    # Recursively search for VHDL files in all subdirectories
+    # Recursively search for plaintext files in all subdirectories
     for root, dirs, files in os.walk(current_directory):
-        vhdl_files.extend([os.path.join(root, file) for file in files if file.endswith(FILE_EXTENSION)])
+        plaintext_files.extend([os.path.join(root, file) for file in files if file.endswith(FILE_EXTENSION)])
 
-    # Check if there are any VHDL files
-    if not vhdl_files:
-        print("No VHDL files found in the current directory or its subdirectories.")
+    # Check if there are any plaintext files
+    if not plaintext_files:
+        print("No plaintext files found in the current directory or its subdirectories.")
         return
 
     # Create a new Word document
     document = Document()
 
-    # Add a custom style for VHDL code
+    # Add a custom style for plaintext code
     style = document.styles.add_style('Code', WD_PARAGRAPH_ALIGNMENT.CENTER)
     font = style.font
     font.name = 'Courier New'
     font.size = Pt(10)
 
-    # Loop through each VHDL file and add its content to the document
-    for vhdl_file in vhdl_files:
-        add_vhdl_to_docx(document, vhdl_file)
+    # Loop through each plaintext file and add its content to the document
+    for plaintext_file in plaintext_files:
+        add_plaintext_to_docx(document, plaintext_file)
 
     # Save the document
     document.save(output_file_name)
 
     print(f"Merge complete. Merged content saved in {output_file_name}.")
 
-# Call the function to merge VHDL files
-merge_vhdl_files()
+# Call the function to merge plaintext files
+merge_plaintext_files()
